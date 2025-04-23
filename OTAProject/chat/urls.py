dada3de58e3bfcda1586bda6cc16667abc42dc8a
skad13 <2273387741@qq.com>
2025-04-23 09:33:10 +0800

    Delete OTAProject directory

--- OTAProject/chat/urls.py
+++
@@ -1,8 +0,0 @@
-# OTAProject/chat/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('api/contacts/', views.chat_contacts_api, name='chat_contacts_api'),
-    path('api/history/', views.chat_history_api, name='chat_history_api'),
-]
